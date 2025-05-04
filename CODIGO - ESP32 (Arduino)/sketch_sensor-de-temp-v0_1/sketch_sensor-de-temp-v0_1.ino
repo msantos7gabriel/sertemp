a7a621bed5f961ae6163a15abf42048f3b569a84
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2025-05-04 18:32:53 -0300

    adicionado gráficos de informação em função do tempo

--- CODIGO - ESP32 (Arduino)/sketch_sensor-de-temp-v0_1/sketch_sensor-de-temp-v0_1.ino
+++ CODIGO - ESP32 (Arduino)/sketch_sensor-de-temp-v0_1/sketch_sensor-de-temp-v0_1.ino
@@ -1,22 +1,24 @@
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
 #include <DHT.h>
-// #include <Adafruit_BMP085.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <time.h>
 
-// Adafruit_BMP085 bmp;
 DHT dht(2, DHT11);
 
 #define WIFI_SSID "Vitor Repetidor "
 #define WIFI_PASSWORD "cimentos"
-
 #define FIREBASE_HOST "https://sertemp-651ae-default-rtdb.firebaseio.com/" 
 #define FIREBASE_API_KEY "AIzaSyAHZos40wQIftXTDs-1_yFKNhCpnGtTwIA" 
-
 #define USER_EMAIL "admin@gmail.com"
 #define USER_PASS "admin1977"
 
-FirebaseData fbdo;
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = -3 * 3600;
+const int daylightOffset_sec = 0;
 
+FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
@@ -24,86 +26,84 @@ unsigned long sendDataPrevMillis = 0;
 
 void setup() {
   Serial.begin(115200);
-
-  // if (!bmp.begin()){
-  //   Serial.println("Could not find BMP180. Check wiring!");
-  //   delay(100);
-  // }
-
   dht.begin();
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Conectando WiFi...");
-
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Status: ");
-    Serial.println(WiFi.status());
-    Serial.print("Target: ");
-    Serial.println(WL_CONNECTED);
     delay(500);
+    Serial.print(".");
   }
   Serial.println("\nConectado! IP: " + WiFi.localIP().toString());
 
-  config.database_url = "https://" + String(FIREBASE_HOST);
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  
+  config.database_url = FIREBASE_HOST;
   config.api_key = FIREBASE_API_KEY;
-
   auth.user.email = USER_EMAIL;
   auth.user.password = USER_PASS;
-
-  config.database_url = FIREBASE_HOST;
-
+  
   Firebase.reconnectNetwork(true);
-
   Firebase.begin(&config, &auth);
-
   Firebase.setDoubleDigits(5);
-
   config.timeout.serverResponse = 10 * 1000;
-
 }
 
 void loop() {
-  // Serial.print("Altitude: ");
-  // Serial.println(bmp.readAltitude());
-  // Serial.print("Pressão: ");
-  // Serial.println(bmp.readSealevelPressure());
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0)) {
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 60000 || sendDataPrevMillis == 0)) {
     sendDataPrevMillis = millis();
-    // long int altitude = bmp.readAltitude();
-    // long int pressure = bmp.readSealevelPressure();
+    
     float temp = dht.readTemperature();
     float humidity = dht.readHumidity();
-
-    // if (Firebase.RTDB.setInt(&fbdo, "ceraima/Altitude", altitude)){
-    //   Serial.print("ALtitude: ");
-    //   Serial.println(altitude);
-    // } else {
-    //   Serial.println(fbdo.errorReason().c_str());
-    // }
-
-    // if (Firebase.RTDB.setInt(&fbdo, "ceraima/SealevelPressure", pressure)){
-    //   Serial.print("Pressure: ");
-    //   Serial.println(pressure);
-    // } else {
-    //   Serial.println(fbdo.errorReason().c_str());
-    // }
-
-    if (Firebase.RTDB.setInt(&fbdo, "ceraima/Temperature", temp)){
-      Serial.print("Teperature: ");
-      Serial.println(temp);
-    } else {
-      Serial.println(fbdo.errorReason().c_str());
+    
+    if (isnan(temp) || isnan(humidity)) {
+      Serial.println("Falha na leitura do sensor DHT!");
+      return;
     }
-
-    if (Firebase.RTDB.setInt(&fbdo, "ceraima/Humidity", humidity)){
-      Serial.print("Humidity: ");
-      Serial.println(humidity);
-    } else {
-      Serial.println(fbdo.errorReason().c_str());
+    
+    struct tm timeinfo;
+    if(!getLocalTime(&timeinfo)){
+      Serial.println("Falha ao obter o horário");
+      return;
     }
-
-  } else {
-    Serial.println(".");
+    
+    char dateTime[30];
+    strftime(dateTime, sizeof(dateTime), "%Y-%m-%d_%H:%M:%S", &timeinfo);
+    
+    char dateOnly[15];
+    strftime(dateOnly, sizeof(dateOnly), "%Y-%m-%d", &timeinfo);
+    
+    char timeOnly[10];
+    strftime(timeOnly, sizeof(timeOnly), "%H", &timeinfo);
+    
+    Serial.print("Data e hora: ");
+    Serial.println(dateTime);
+    
+    FirebaseJson json;
+    json.set("temperature", temp);
+    json.set("humidity", humidity);
+    json.set("timestamp", dateTime);
+    
+    String path = "ceraima/historico/" + String(dateOnly) + "/" + String(timeOnly);
+    
+    if (Firebase.RTDB.setJSON(&fbdo, path.c_str(), &json)) {
+      Serial.println("Dados gravados com sucesso em: " + path);
+    } else {
+      Serial.println("ERRO: " + fbdo.errorReason());
+    }
+    
+    Firebase.RTDB.setFloat(&fbdo, "ceraima/Temperature", temp);
+    Firebase.RTDB.setFloat(&fbdo, "ceraima/Humidity", humidity);
+    Firebase.RTDB.setString(&fbdo, "ceraima/lastUpdate", dateTime);
+    
+    Serial.println("Temperatura: " + String(temp) + "°C");
+    Serial.println("Umidade: " + String(humidity) + "%");
   }
-  delay(1000);
+
+  const int sec = 1000;
+  const int min = 60 * sec;
+  const int hour = 60 * min;
+  const int delay_in_sec = hour;
+  
+  delay(delay_in_sec);
 }
